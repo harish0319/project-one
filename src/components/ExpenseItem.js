13092a5aff8f954fc363fb2101b23c88356a9609
hbehera27 <harishb2727@gmail.com>
2025-04-04 09:44:24 +0530

    project setup changes in app.js file

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+        <div>
+          <li>Food-$5</li>
+          <li>Petrol-$20</li>
+          <li>Movie-$10</li> 
+        </div>  
+      );
+}
+
+export default ExpenseItem;

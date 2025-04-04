1eceddb00110610d5de283146ddaaf90b1177680
hbehera27 <harishb2727@gmail.com>
2025-04-04 11:42:55 +0530

    Styling Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return (
-      <div>
-        <div>15 August  2023</div>
-        <div>
-          <h2>Book</h2>
-          <div>$10</div>
-        </div>
-        <div>Delhi</div>
+  return (
+    <div className="expense-item">
+      <div>15 August 2023</div>
+      <div className="expense-item__description">
+        <h2>Book</h2>
+        <div className="expense-item__price">$10</div>
       </div>
-    );
-  }
-  
-  export default ExpenseItem;
+      <div className="expense-item__location">Delhi</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;

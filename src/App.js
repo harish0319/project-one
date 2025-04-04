d6bc16ccb0a6b43913ddfc3d33d1a58af8d14a3e
hbehera27 <harishb2727@gmail.com>
2025-04-04 19:45:47 +0530

    props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ function App() {
   return (
     <div>
       <h1>Let's get Started</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 7, 15)}
+        location="Bangalore"
+        title="Insurance"
+        price="50"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 3, 25)}
+        location="Delhi"
+        title="Book"
+        price="20"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 10, 11)}
+        location="Hyderabad"
+        title="Pen"
+        price="5"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 1, 14)}
+        location="Mumbai"
+        title="Laptop"
+        price="200"
+      ></ExpenseItem>
     </div>
   );
 }

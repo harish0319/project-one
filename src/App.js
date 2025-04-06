d801514e26f531bc6efa28f2bbc937c8fdaba04d
hbehera27 <harishb2727@gmail.com>
2025-04-06 10:13:30 +0530

    Nested Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import Expenses from "./components/Expenses";
 function App() {
-  
+  const expenses = [
+    { id: "1", date: new Date(2023, 7, 15), location:"Bangalore",title: "Insurance", price: 100 },
+    { id: "2", date: new Date(2023, 3, 25), location:"Delhi",title: "Book", price: 10 },
+    { id: "3", date: new Date(2023, 10, 11), location:"Hyderabad",title: "Pen", price: 1 },
+    { id: "4", date: new Date(2023, 1, 14), location:"Mumbai",title: "Laptop", price: 200 },
+  ];  
 
   return (
     <div>
       <h1>Let's get Started</h1>
-      <Expenses />
+      <Expenses expenses={expenses}/>
     </div>
   );
 }

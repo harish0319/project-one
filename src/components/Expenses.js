e6719f411cad340d962d4428d722e22781c393b6
hbehera27 <harishb2727@gmail.com>
2025-04-06 10:37:48 +0530

    Wrapper Component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import Card from "./Card"; 
 
 function Expenses(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {props.expenses.map((expense) => (
                 <ExpenseItem
                     key={expense.id}
@@ -13,7 +14,7 @@ function Expenses(props) {
                     price={expense.price}
                 />
             ))}
-        </div>
+        </Card>
     );
 }
 

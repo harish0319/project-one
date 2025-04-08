5099061b51dbd0819ccb578a69d8e94509be8b98
hbehera27 <harishb2727@gmail.com>
2025-04-08 08:28:56 +0530

    managing multiple state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Expenses from "./components/Expenses/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
   const expenses = [
     { id: "1", date: new Date(2023, 7, 15), location:"Bangalore",title: "Insurance", price: 100 },
@@ -11,7 +11,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Let's get Started</h1>
+      <NewExpense/>
       <Expenses expenses={expenses}/>
     </div>
   );

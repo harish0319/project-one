5d598aef666ff3171b638be3c16217e6b68ae0ed
hbehera27 <harishb2727@gmail.com>
2025-04-09 20:55:53 +0530

    conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,24 +11,37 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  // Filter expenses based on selected year
   const filteredExpenses = props.expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  return (
-    <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={changeFilterHandler} />
-      {filteredExpenses.map((expense) => {
-        return (
-          <ExpenseItem
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => {
+      return (
+        <>
+            <ExpenseItem
             key={expense.id}
             title={expense.title}
             date={expense.date}
             price={expense.price}
-          />
-        );
-      })}
+            />
+            {filteredExpenses.length === 1 && (
+                <p>Only one expense found. Please add more</p>
+            )}
+        </>
+      );
+    });
+  }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      {expensesContent}
     </Card>
   );
 };
